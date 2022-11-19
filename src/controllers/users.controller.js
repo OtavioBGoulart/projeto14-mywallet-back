d466b8969614ad044d873f28b8dacdc5687e96a8
Otavio Goulart <otaviobgoulartt@gmail.com>
2022-11-19 01:50:27 -0300

    feat: sing-up users.controller

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -1,6 +1,22 @@
 import bcrypt from "bcrypt"
+import { func } from "joi";
 import { v4 as uuidV4 } from "uuid";
 import { usersCollection, sessionsCollection } from "../database/db";
+
+
+export async function singUp(req, res) {
+    const user = req.body;
+    delete user.body.confirmedPass
+
+    try {
+        const hashPassword = bcrypt.hashSync(user.password, 11);
+        await usersCollection.insertOne({...user, password: hashPassword});
+        res.sendStatus(201);
+    } catch(error) {
+        console.log(error);
+        res.sendStatus(500);
+    }
+}
 
 export async function singIn(res, res) {
     const { email } = req.body;
