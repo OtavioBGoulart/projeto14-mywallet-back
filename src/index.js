52122abd0774584e2985d804c256e28599575267
Otavio Goulart <otaviobgoulartt@gmail.com>
2022-11-19 20:07:26 -0300

    feat: app use routes

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import express from "express";
 import cors from "cors";
 import usersRouters from "./routes/users.routes.js";
+import transactionRouters from "./routes/transactions.routes.js"
 
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(usersRouters);
+app.use(transactionRouters);
 
 
 app.listen(5000, () => console.log("Server running in port 5000"));
